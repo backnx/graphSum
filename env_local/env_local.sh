b5d10ee6d43767eef7de04ad704a33f1f9d1bdb4
Bach Ngo <bachssun123@gmail.com>
2022-12-17 22:30:35 +0700

    first commits

---
+++ env_local/env_local.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+set -xe
+
+#export iplist=10.255.104.19,10.255.138.19,10.255.122.21
+export iplist=183.91.3.60 # ip of the local machine
+
+#http_proxy
+unset http_proxy
+unset https_proxy
